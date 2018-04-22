821149bfea68bcb4f243f015d2a053e562140fb8
Charles Martins <charleshenriquems@gmail.com>
2018-04-22 19:39:24 -0300

    versao com mais passos.

--- desafio12.py
+++ desafio12.py
@@ -4,5 +4,5 @@
 Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
 '''
 preco = float(input('Digite o preço do produto: '))
-print('O valor {:.2f} com desconto de 5%  vai custar {:.2f}'.format(preco, 0.95*preco))
- 
+#print('O valor {:.2f} com desconto de 5%  vai custar {:.2f}'.format(preco, 0.95*preco))
+print('O valor {:.2f} com desconto de 5% vai custar {:.2f}.format(preco, preco-(preco*5/100))')
